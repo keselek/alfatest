25b1768e868aa214951dbabada60335849e01cde
keselek <keselek1@gmail.com>
2016-05-03 21:34:29 +0300

    2

--- core/components/alfatest/elements/plugins/plugin.alfatest.php
+++ core/components/alfatest/elements/plugins/plugin.alfatest.php
@@ -1,5 +1,33 @@
 <?php
 
 switch ($modx->event->name) {
+    case 'OnHandleRequest':
+    case 'OnLoadWebDocument':
+
+
+        $isAjax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+
+        if (
+        empty($_REQUEST['alfatest'])
+        ) {
+            return;
+        }
+
+        $action = trim($_REQUEST['alfatest']);
+
+
+        /** @var Documents $Documents */
+        switch ($action) {
+            case 'test1':
+                $response = 'result';
+                break;
+
+
+
+
+
+        }
+        exit($response);
+
 
 }
